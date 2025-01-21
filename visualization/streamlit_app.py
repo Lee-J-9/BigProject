276b944e3f324fe6de823bf13042eff060fe99b6
Lee-J-9 <jaegu97@gmail.com>
2025-01-21 16:47:01 +0900

    Update streamlit_app.py

--- visualization/streamlit_app.py
+++ visualization/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # HTML 파일 읽기
-html_file_path = "./result/test5.html"  # HTML 파일 경로
+html_file_path = "https://raw.githubusercontent.com/Lee-J-9/BigProject/refs/heads/main/visualization/result/test5.html"  # HTML 파일 경로
 with open(html_file_path, "r", encoding="utf-8") as f:
     html_content = f.read()
 
