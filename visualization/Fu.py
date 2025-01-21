17f75f1b66566e4112e850881453d7a6186c885a
Lee-J-9 <jaegu97@gmail.com>
2025-01-22 01:13:19 +0900

    글자 몇개 업데이트 했는데 되려나

--- visualization/Fu.py
+++ visualization/Fu.py
@@ -2,8 +2,14 @@ import requests
 import streamlit as st
 if name == “main”:
 main()
+# 이거 왜 됨?
+# main 때문인가?
 # GitHub의 raw 파일 URL
 url = 'https://raw.githubusercontent.com/Lee-J-9/BigProject/refs/heads/main/visualization/result/test5.html'
+
+st.title('이게 왜 될까')
+st.subheader('서울시 쓰레기통 지도')
+st.write('추가 쓰레기통 X, 테스트용용')
 # HTML 파일 가져오기
 response = requests.get(url)
 if response.status_code == 200:
