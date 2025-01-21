16c417299e25ca9799ce229f13e79ebe7b182d2c
Lee-J-9 <jaegu97@gmail.com>
2025-01-21 16:43:00 +0900

    에라이

--- visualization/streamlit_app.py
+++ visualization/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # HTML 파일 읽기
-html_file_path = "../visualization/result/test5.html"  # HTML 파일 경로
+html_file_path = "./result/test5.html"  # HTML 파일 경로
 with open(html_file_path, "r", encoding="utf-8") as f:
     html_content = f.read()
 
