cf126fd1fffa19be1b8c088873a89e582a7c476c
잼산자 <summer970@naver.com>
2025-01-22 09:39:53 +0900

    0122

--- visualization/Fu.py
+++ visualization/Fu.py
@@ -7,19 +7,10 @@ import streamlit as st
 # GitHub의 raw 파일 URL
 url = 'https://raw.githubusercontent.com/Lee-J-9/BigProject/refs/heads/main/visualization/result/test5.html'
 
-st.title('이게 왜 될까')
+st.title('안녕하세요 이재구 입니다.')
 st.subheader('서울시 쓰레기통 지도')
 st.write('추가 쓰레기통 X, 테스트용')
-st.markdown(
-    """
-    <style>
-    .stTitle {
-        text-align: center;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 # HTML 파일 가져오기
 response = requests.get(url)
 if response.status_code == 200:
